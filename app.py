52d27b88c66def48649a9be9e14031c451dcac3f
Ahmed-adel23 <121492522+Ahmed-adel23@users.noreply.github.com>
2023-10-14 21:10:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ api.add_resource(MSPrediction3, '/MSPrediction3')
 api.add_resource(MSPrediction4, '/MSPrediction4')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0' , port=8000)
+    app.run(host='0.0.0.0' )
